5ecfd5d2b2b6269f63a2f25daa753b29b834d4cd
medioxor <medioxor@gmail.com>
2024-11-28 18:17:37 +1000

    fix format

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,4 @@ pub mod collection {
     pub mod memory;
     #[path = "rules.rs"]
     pub mod rules;
-    pub mod reader {
-        #[cfg(target_os = "windows")]
-        #[path = "../file/ntfs_reader.rs"]
-        pub mod ntfs_reader;
-    }
 }
